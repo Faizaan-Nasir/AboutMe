45f1ffc37ad2317354e467377ddcd85156f4c9ab
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2023-03-09 11:20:31 +0400

    Updated Blog page, background and navbar

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,9 @@ function home_page() {
     document.getElementById("title").innerHTML = "Home"
     return false
 }
+
+function blog() {
+    document.getElementById("icon").href = "./src/blog.png"
+    document.getElementById("title").innerHTML = "Weblog"
+    return false
+}
